9bb48ddf39e3e2a1fb703cb30f395851d290922b
Manca Murn <manci.murn10@gmail.com>
2021-09-27 19:55:38 +0200

    osnutek modela

--- model.py
+++ model.py
@@ -1,14 +1,55 @@
+from bottle import JSONPlugin
+import random
+
+
 class Uporabnik:
-    def __init__(self, ime):
+    def __init__(self, ime, geslo):
         self.ime = ime
+        self.geslo = geslo
+
+
+
+class Podatek:
+    def __init__(self, baza):
+        self.baza = baza
+    
+    def izberi_vrednost(self):
+        self.vrednost = random.choice(self.baza)
+    
 
 class Naloga:  
-    def __init__(self):
-        self.besedilo
+    def __init__(self, besedilo, st_naloge = 0, st_podatkov = 0):
+        self.besedilo = besedilo
+        self.st_naloge = st_naloge
+        self.st_podatkov = st_podatkov
         self.resitev = self.izracunaj_resitev()
+        self.podatki = self.ustvari_seznam_podatkov()
+
+    # kje preberem st_podatkov
 
     def izracunaj_resitev(self):
         pass
+    # resitev naj bi bila podana kot formula, v katero po vrsti vtavljamo seznam podatkov.
+    # kako preberem formulo?
 
-    def vrni_nakljucne_podatke(self):
-        pass
+    def ustvari_seznam_podatkov(self):
+        seznam = [] 
+        for x in range(self.st_podatkov):
+        #    seznam.append(Podatek(baza))
+        #kako najdem bazo za vsak podatek po vrsti
+            pass
+
+
+
+class Test:
+    def __init__(self, glava, st_testa, st_nalog, seznam_nalog):
+        self.glava = glava
+        self.st_testa = st_testa
+        self.st_nalog = st_nalog
+        self.naloge = self.ustvari_seznam_nalog()
+    
+    def ustvari_seznam_nalog(self):
+        seznam = []
+        for x in range(self.st_nalog):
+        #    seznam.append(Naloga(besedilo, x, st_podatkov)
+            pass
