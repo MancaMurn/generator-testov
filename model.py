761c2924f298d7fcfadd2b26ade567009486cbea
Manca Murn <manci.murn10@gmail.com>
2021-09-28 08:59:22 +0200

    nadaljevanje modela

--- model.py
+++ model.py
@@ -2,54 +2,80 @@ from bottle import JSONPlugin
 import random
 
 
+
+
 class Uporabnik:
     def __init__(self, ime, geslo):
         self.ime = ime
         self.geslo = geslo
 
 
+# V besedilo naj manjkajoč podatek označijo z #. Besedilo naj bo v obliki niza.
+# V kakšni obliki naj bo formula za rešitev?
 
-class Podatek:
-    def __init__(self, baza):
-        self.baza = baza
-    
-    def izberi_vrednost(self):
-        self.vrednost = random.choice(self.baza)
-    
 
 class Naloga:  
-    def __init__(self, besedilo, st_naloge = 0, st_podatkov = 0):
-        self.besedilo = besedilo
-        self.st_naloge = st_naloge
-        self.st_podatkov = st_podatkov
-        self.resitev = self.izracunaj_resitev()
-        self.podatki = self.ustvari_seznam_podatkov()
+    def __init__(self, besedilo = "", st_razlicic = 0, slovar_podatkov = {}, formula_resitve = ""):
+        self.st_razlicic = st_razlicic #izberemo na zacetku koliko razlicic testov zelimo. vsaka naloga v enem testu ima ta atribut enak.
+        
+        self.besedilo = besedilo    #besedilo podamo v obliki niza
+        self.st_podatkov = self.besedilo.count("#")
+        
+        self.formula = formula_resitve #kako naj bo podana formula??
 
-    # kje preberem st_podatkov
+        self.slovar = slovar_podatkov #baze naj bi izbrali s klikom, kako to spravim v slovar??
 
-    def izracunaj_resitev(self):
+
+    def izberi_seznam_podatkov(self):
+        seznam = [] 
+        for x in self.slovar:
+            seznam.append(random.coice(self.slovar[x]))
+        return seznam
+    # funkcija iz slovarja podatkov z bazami izbere naključne podatke in jih sharni v seznam.
+
+
+    def izracunaj_resitev(self, seznam_podatkov):
         pass
     # resitev naj bi bila podana kot formula, v katero po vrsti vtavljamo seznam podatkov.
     # kako preberem formulo?
 
-    def ustvari_seznam_podatkov(self):
-        seznam = [] 
-        for x in range(self.st_podatkov):
-        #    seznam.append(Podatek(baza))
-        #kako najdem bazo za vsak podatek po vrsti
-            pass
+
+    def oblikuj_nalogo(self, seznam_podatkov):
+        besedilo = self.besedilo
+        podatki = seznam_podatkov
+        n = 0
+        for x in besedilo:
+            if x == "#":
+                besedilo.replace("#", str(podatki[0]))
+                n += 1 
+        return besedilo
+    #funkcija naj bi v besedilo vstavila podatke
+
+
+    def ustvari_razlicice(self):
+        slovar_razlicic = {}
+        for x in range(self.st_razlicic):
+            podatki = self.izberi_seznam_podatkov()
+            resitev = self.izracunaj_resitev(podatki)
+
+            slovar_razlicic[self.oblikuj_nalogo(podatki)] = resitev
+    # funcija v slovar shrani razlicice naloge in resitve
+
+
+
 
 
 
 class Test:
-    def __init__(self, glava, st_testa, st_nalog, seznam_nalog):
+    def __init__(self, glava, st_razlicic, st_nalog, seznam_nalog):
         self.glava = glava
-        self.st_testa = st_testa
-        self.st_nalog = st_nalog
+        self.st_razlicic = st_razlicic
+        self.st_nalog = st_nalog        #kako preberem st_nalog??
         self.naloge = self.ustvari_seznam_nalog()
     
     def ustvari_seznam_nalog(self):
         seznam = []
         for x in range(self.st_nalog):
-        #    seznam.append(Naloga(besedilo, x, st_podatkov)
             pass
+
+        #kako ustvarim seznam nalog --> to bi bil potem seznam slovarjev razlicic nalog
