94254ca2dc99e7909061fab7304b282ab4ef7de4
Manca Murn <manci.murn10@gmail.com>
2021-09-29 16:44:59 +0200

    dopolnjen model

--- model.py
+++ model.py
@@ -1,21 +1,47 @@
 from bottle import JSONPlugin
 import random
 
-
-
-
 class Uporabnik:
     def __init__(self, ime, geslo):
         self.ime = ime
         self.geslo = geslo
 
+        self.seznam_nalog = []
+
+    def shrani_uporabnika(self):
+        # Poglej pretnarjevo implementacijo
+        with open("uporabniki.json"):
+            pass
+    
+    def nalozi_uporabnika(self, uporabnisko_ime):
+        # Poglej pretnarjevo implementacijo
+        json.load("ime datoteke")
+
+
 
 # V besedilo naj manjkajoč podatek označijo z #. Besedilo naj bo v obliki niza.
 # V kakšni obliki naj bo formula za rešitev?
 
+class Razlicica:
+    def __init__(self, formula_resitve, besedilo = "", slovar_podatkov = None):
+        self.besedilo = besedilo
+        self.resitev = self.izracunaj_resitev(formula_resitve)
+        self.slovar_podatkov = slovar_podatkov # Oblike npr. {"#1" : 5, "#2" : 7, "#7" : 6}
+    
+    def izracunaj_resitev(self, formula):
+        for spremenljivka in self.slovar_podatkov:
+            formula = formula.replace(spremenljivka, str(self.slovar_podatkov[spremenljivka]))
+        
+        return eval(formula)
+
+
+# x = Naloga("Besedilo # asdfasdf # asfdasdf", 5)
+# for razlicica in x.seznam_razlicic:
+#     print(razlicica.besedilo)   
+#     print(razlicica.resitev)
 
 class Naloga:  
-    def __init__(self, besedilo = "", st_razlicic = 0, slovar_podatkov = {}, formula_resitve = ""):
+    def __init__(self, besedilo = "", st_razlicic = 0, slovar_podatkov = None, formula_resitve = ""):
         self.st_razlicic = st_razlicic #izberemo na zacetku koliko razlicic testov zelimo. vsaka naloga v enem testu ima ta atribut enak.
         
         self.besedilo = besedilo    #besedilo podamo v obliki niza
@@ -23,47 +49,39 @@ class Naloga:
         
         self.formula = formula_resitve #kako naj bo podana formula??
 
-        self.slovar = slovar_podatkov #baze naj bi izbrali s klikom, kako to spravim v slovar??
+        self.slovar = slovar_podatkov or dict() #baze naj bi izbrali s klikom, kako to spravim v slovar??
+        self.seznam_razlicic = []
 
-
-    def izberi_seznam_podatkov(self):
-        seznam = [] 
-        for x in self.slovar:
-            seznam.append(random.coice(self.slovar[x]))
-        return seznam
+    def izberi_podatke(self):
     # funkcija iz slovarja podatkov z bazami izbere naključne podatke in jih sharni v seznam.
+        slovar = {} 
+        for x in self.slovar:
+            slovar[x] = random.coice(self.slovar[x])
+        return slovar  
+        
 
+    def vstavi_podatke_v_besedilo(self, podatki):
+    # funkcija naj bi v besedilo vstavila podatke
+    # podatki so slovar oblike npr. {"#1" : 5, "#2" : 7, "#7" : 6}
 
-    def izracunaj_resitev(self, seznam_podatkov):
-        pass
-    # resitev naj bi bila podana kot formula, v katero po vrsti vtavljamo seznam podatkov.
-    # kako preberem formulo?
+        novo_besedilo = self.besedilo
 
-
-    def oblikuj_nalogo(self, seznam_podatkov):
-        besedilo = self.besedilo
-        podatki = seznam_podatkov
-        n = 0
-        for x in besedilo:
-            if x == "#":
-                besedilo.replace("#", str(podatki[0]))
-                n += 1 
-        return besedilo
-    #funkcija naj bi v besedilo vstavila podatke
+        for spremenljivka in podatki:
+            novo_besedilo = novo_besedilo.replace(spremenljivka, str(podatki[spremenljivka]))
+        
+        return novo_besedilo
 
 
     def ustvari_razlicice(self):
-        slovar_razlicic = {}
+    # funcija v seznam shrani razlicice naloge in resitve
+    # resitve so pol v klasu razlicica
+
         for x in range(self.st_razlicic):
-            podatki = self.izberi_seznam_podatkov()
-            resitev = self.izracunaj_resitev(podatki)
+            slovar_podatkov = self.izberi_podatke()
+            besedilo = self.vstavi_podatke_v_besedilo(slovar_podatkov)
 
-            slovar_razlicic[self.oblikuj_nalogo(podatki)] = resitev
-    # funcija v slovar shrani razlicice naloge in resitve
-
-
-
-
+            razlicica = Razlicica(self.formula, besedilo, slovar_podatkov)
+            self.seznam_razlicic.append(razlicica)
 
 
 class Test:
--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -3,16 +3,23 @@ from model import Uporabnik
 # from model import *
 
 x = None
+y = None
 
 @bottle.get("/")
 def zacetna_stran():
-    return bottle.template("hello_world.html", uporabnik=x)
+    if x:
+        a = x.ime
+    else:
+        a = None
+
+    return bottle.template("prijava.html", napaka = None, uporabnisko_ime = a)
 
 @bottle.post("/")
 def zacento_ime():
-    uporabnisko_ime = bottle.request.forms.get("uporabniskoIme")
+    uporabnisko_ime = bottle.request.forms.get("uporabnisko_ime")
+    geslo = bottle.request.forms.get("geslo")
     global x
-    x = Uporabnik(uporabnisko_ime)
+    x = Uporabnik(uporabnisko_ime, geslo)
     bottle.redirect("/") #GET
 
 if __name__ == "__main__":
