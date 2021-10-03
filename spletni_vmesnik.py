f8dad850ae445edaf7c18494292fa490e883b197
Manca Murn <manci.murn10@gmail.com>
2021-10-03 17:31:21 +0200

    spletni vmesnik

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,6 +1,10 @@
 import bottle
 from model import Uporabnik
 # from model import *
+import model
+
+PISKOTEK_UPORABNISKO_IME = "uporabnisko_ime"
+SKRIVNOST = "to je ena skrivnost"
 
 x = None
 y = None
@@ -9,25 +13,62 @@ y = None
 def zacetna_stran():
     return bottle.template("prva_stran.html", napaka=None)
 
-@bottle.post("/")
-def zacento_ime():
-    uporabnisko_ime = bottle.request.forms.get("uporabnisko_ime")
-    geslo = bottle.request.forms.get("geslo")
-    global x
-    x = Uporabnik(uporabnisko_ime, geslo)
-    bottle.redirect("/") #GET
+# @bottle.post("/")
+# def zacentno_ime():
+#     uporabnisko_ime = bottle.request.forms.get("uporabnisko_ime")
+#     geslo = bottle.request.forms.get("geslo")
+#     global x #kaj je ta global??
+#     x = Uporabnik(uporabnisko_ime, geslo, seznam_testov=[])
 
-@bottle.get("/prijava/")
-def prijava():
-    return bottle.template("prijava.html", napaka=None)
+#     return bottle.template("pozdrav_uporabnika.html", uporabnisko_ime, napaka = None)
+
+@bottle.post("/prijava/")
+def prijava_post():
+    uporabnisko_ime = bottle.request.forms.getunicode("uporabnisko_ime")
+    geslo_v_cistopisu = bottle.request.forms.getunicode("geslo")
+    if not uporabnisko_ime:
+        return bottle.template("registracija.html", napaka="Vnesi uporabniško ime!")
+    try:
+        Uporabnik.prijava(uporabnisko_ime, geslo_v_cistopisu)
+        bottle.response.set_cookie(
+            PISKOTEK_UPORABNISKO_IME, uporabnisko_ime, path="/", secret=SKRIVNOST
+        )
+        bottle.redirect("/")
+    except ValueError as e:
+        return bottle.template(
+            "prijava.html", napaka=e.args[0]
+        )
+
+
 
 @bottle.get("/registracija/")
 def registracija():
     return bottle.template("registracija.html", napake=[])
 
+
+
+@bottle.post("/registracija/")
+def registracija_post():
+    uporabnisko_ime = bottle.request.forms.getunicode("uporabnisko_ime")
+    geslo_v_cistopisu = bottle.request.forms.getunicode("geslo")
+    if not uporabnisko_ime:
+        return bottle.template("registracija.html", napaka="Vnesi uporabniško ime!")
+    try:
+        Uporabnik.registracija(uporabnisko_ime, geslo_v_cistopisu)
+        bottle.response.set_cookie(
+            PISKOTEK_UPORABNISKO_IME, uporabnisko_ime, path="/", secret=SKRIVNOST
+        )
+        bottle.redirect("/")
+    except ValueError as e:
+        return bottle.template(
+            "registracija.html", napaka=e.args[0]
+        )
+
+
 @bottle.route('/static/<filename:path>')
 def send_static(filename):
     return bottle.static_file(filename, root='static')
+
 
 if __name__ == "__main__":
     bottle.run(debug=True, reloader=True)
